ac178db202b5a48790ab424258926f10e11dd7a6
EdsonCarneiro <edsonrje@gmail.com>
2022-07-26 18:20:05 -0300

    Resolução 009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString2 = "Eu não sou supersticioso, mas sou um pouco ________."
+console.log(minhaString.length, minhaString2.length);
+
+const minhaString3 = "Eu não sou supersticioso, mas sou um pouco sticioso."
+
+
+
